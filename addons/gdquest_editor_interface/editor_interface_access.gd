f620cb0dddb704f55d076f867df589c69ba0f66a
Yuri Sizov <yuris@humnom.net>
2026-03-18 15:49:59 +0100

    Improve inline documentation, add dock helper method

--- addons/gdquest_editor_interface/editor_interface_access.gd
+++ addons/gdquest_editor_interface/editor_interface_access.gd
@@ -1,5 +1,5 @@
-## Editor interface access is a system for serializing, referencing, and retrieving
-## editor GUI elements with persistent identifiers.
+## Editor interface access is a system for serializing, referencing, and
+## retrieving editor GUI elements with persistent identifiers.
 @tool
 
 const Enums := preload("./utils/eia_enums.gd")
@@ -8,38 +8,37 @@ const Resolver := preload("./utils/eia_resolver.gd")
 
 # Node access.
 
-## Resolves and returns a node in the editor tree associated with the
-## given node point value. Returns null if the node, or one of its
+## Resolves and returns a node in the editor tree associated with the given
+## node point value. Returns [code]null[/code] if the node, or one of its
 ## pre-requisites, cannot be resolved.
 ##
-## Resolved node is cached for future calls, unless the skip_cache
+## Resolved node is cached for future calls, unless the [param skip_cache]
 ## flag is set.
 static func get_node(node_point: Enums.NodePoint, skip_cache: bool = false) -> Node:
 	return Resolver.resolve_node(node_point, null, skip_cache)
 
 
-## Resolves and returns a node in the editor tree associated with the
-## given node point value, relative to the given base node. If the
-## definition for this node point has a base reference, it will be ignored.
-## Returns null if the node, or one of its pre-requisites, cannot be resolved.
+## Resolves and returns a node in the editor tree associated with the given
+## node point value, relative to the given base node. Returns [code]null[/code]
+## if the node, or one of its pre-requisites, cannot be resolved.
 ##
-## Resolved node is cached for future calls, relative to the provided
-## base_node, unless the skip_cache flag is set.
+## Resolved node is cached for future calls, in association with the given
+## base node, unless the [param skip_cache] flag is set.
 static func get_node_relative(base_node: Node, node_point: Enums.NodePoint, skip_cache: bool = false) -> Node:
 	return Resolver.resolve_node(node_point, base_node, skip_cache)
 
 
 # Helpers.
 
-
-## Returns one of the 3D viewports from the 3D editor. Note, that
-## the index does not necessarily correspond to the order in which
-## viewports are made visible. I.e. the viewport at index 1 does
-## not necessarily become visible in the 2-viewport split mode.
+## Returns one of the 3D viewports from the 3D editor.
 ##
-## These viewports also do not container the edited scene. Instead,
-## you can use EditorInterface.get_edited_scene_root() and go up
-## from there. You'll end up in the viewport of the CanvasItemEditor.
+## [b]Note:[/b] The index does not necessarily correspond to the order in
+## which viewports are made visible. I.e. the viewport at index [code]1[/code]
+## does not necessarily become visible in the 2-viewport split mode.
+##
+## These viewports also do not contain the edited scene. Instead, you can use
+## [method EditorInterface.get_edited_scene_root] and go up from there. You'll
+## end up in the viewport of the [CanvasItemEditor].
 static func get_node_3d_viewport(viewport_index: int) -> Control:
 	var viewports_container := get_node(Enums.NodePoint.NODE_3D_EDITOR_VIEWPORTS)
 	var viewports := viewports_container.find_children("", "Node3DEditorViewport", false, false)
@@ -50,8 +49,8 @@ static func get_node_3d_viewport(viewport_index: int) -> Control:
 	return viewports[viewport_index]
 
 
-## Reports if the script editor is currently in focus, whether it
-## is embedded or floating.
+## Returns [code]true[/code] if the script editor is currently in focus, whether
+## it is embedded or floating.
 static func is_script_editor_active() -> bool:
 	var script_editor := get_node(Enums.NodePoint.SCRIPT_EDITOR)
 	if not script_editor:
@@ -71,8 +70,8 @@ static func is_script_editor_active() -> bool:
 	return false
 
 
-## Returns one of the script editor tabs, which can be a TextEditor,
-## a ScriptTextEditor, or an EditorHelp page.
+## Returns one of the script editor tabs, which can be a [TextEditor], a
+## [ScriptTextEditor], or an [EditorHelp] page.
 static func get_script_editor_tab(tab_index: int) -> Control:
 	# NOTE: Script editor tabs at first exist in an "uninstantiated" mode,
 	# until the user clicks on it and the actual contents are spawned. It's
@@ -86,10 +85,15 @@ static func get_script_editor_tab(tab_index: int) -> Control:
 	return script_tabs.get_tab_control(tab_index)
 
 
-## Returns the TabBar of the container currently holding the given
-## dock. If the dock is currently hidden, returns null. However,
-## even hidden docks are inside of the editor node tree, as of 4.6.
+## Returns the [TabBar] control of the container currently holding the
+## given dock. Returns [code]null[/code] if the dock is currently hidden.
+##
+## [b]Note:[/b] As of Godot 4.6, even hidden docks are present inside of
+## the editor node tree, in a special, invisible node.
 static func get_dock_tabs(dock_node: EditorDock) -> TabBar:
+	if not dock_node:
+		return null
+
 	var dock_owner := dock_node.get_parent()
 	if not dock_owner || not ClassDB.is_parent_class(dock_owner.get_class(), "TabContainer"):
 		return null
@@ -97,12 +101,26 @@ static func get_dock_tabs(dock_node: EditorDock) -> TabBar:
 	return (dock_owner as TabContainer).get_tab_bar()
 
 
+## Returns the tab index for the given dock in the container that currently
+## holds it. Returns [code]-1[/code] if the dock is currently hidden.
+## See also [method get_dock_tabs].
+static func get_dock_tab_index(dock_node: EditorDock) -> int:
+	if not dock_node:
+		return -1
+
+	var dock_owner := dock_node.get_parent()
+	if not dock_owner || not ClassDB.is_parent_class(dock_owner.get_class(), "TabContainer"):
+		return -1
+
+	return (dock_owner as TabContainer).get_tab_idx_from_control(dock_node)
+
+
 # Testing.
 
-## Runs resolve for every known node point and reports success
-## status. Uses cache by default, which is necessary for some custom
-## resolver steps. This means that sometimes errors for one node
-## can appear in logs for another due to dependencies.
+## Runs resolution for every known node point and reports success status.
+## Uses cache by default, which is necessary for some custom resolver
+## steps. This means that sometimes errors for one node can appear in logs
+## for another due to dependencies.
 static func test_resolve(skip_cache: bool = false) -> void:
 	print("[EIA] Running resolve test...")
 	print("========================================")
